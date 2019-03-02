87eca4eabfaefd54a45ea8ddcb0055d1d636a005
code-tom-code <48116778+code-tom-code@users.noreply.github.com>
2019-03-02 01:47:47 -0800

    Minor optimization: Caching surface level 0 on the texture object for quick and easy access for UpdateTexture(), Clear(), and texture sampling without mipmapping.

--- Software_d3d9/IDirect3DTexture9Hook.h
+++ Software_d3d9/IDirect3DTexture9Hook.h
@@ -8,7 +8,7 @@ class IDirect3DTexture9Hook : public IDirect3DTexture9
 {
 public:
 	IDirect3DTexture9Hook(LPDIRECT3DTEXTURE9 _realObject, IDirect3DDevice9Hook* _parentDevice) : realObject(_realObject), parentDevice(_parentDevice), refCount(1), AutoGenFilter(D3DTEXF_LINEAR), 
-		surfaceCountMinusOne(0), surfaceCountMinusTwo(0), surfaceCountMinusOneF(0.0f)
+		surfaceCountMinusOne(0), surfaceCountMinusTwo(0), surfaceCountMinusOneF(0.0f), surfaceLevel0(NULL)
 #ifdef DUMP_TEXTURES_ON_FIRST_SET
 		, dumped(0)
 #endif
@@ -103,6 +103,8 @@ protected:
 	UINT surfaceCountMinusOne;
 	INT surfaceCountMinusTwo;
 	float surfaceCountMinusOneF;
+
+	IDirect3DSurface9Hook* surfaceLevel0;
 
 	std::vector<IDirect3DSurface9Hook*> surfaces;
 };
