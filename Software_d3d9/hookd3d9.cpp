a01cad28bdc79c3ad08bdc711bd379ad2ebf713c
code-tom-code <48116778+code-tom-code@users.noreply.github.com>
2019-03-04 22:58:38 -0800

    Cleaning up the formatting in hookd3d9.cpp and adding some new comments.

--- Software_d3d9/hookd3d9.cpp
+++ Software_d3d9/hookd3d9.cpp
@@ -161,10 +161,10 @@ void CircumventSteamAntiDebugging(void)
 }
 #endif
 
-__declspec(dllexport) BOOL WINAPI DllMain(HINSTANCE hInst,DWORD reason,LPVOID)
-	{
+__declspec(dllexport) BOOL WINAPI DllMain(_In_ HINSTANCE hInst, _In_ DWORD reason, _In_ LPVOID /*lpvReserved*/)
+{
 	if (reason == DLL_PROCESS_ATTACH)
-		{
+	{
 		hLThisDLL = hInst;
 
 #ifdef _DEBUG
@@ -172,186 +172,186 @@ __declspec(dllexport) BOOL WINAPI DllMain(HINSTANCE hInst,DWORD reason,LPVOID)
 #endif
 
 		// x86 32-bit version:
+		// TODO: Don't hardcode this path
 		hL = LoadLibraryA("C:\\Windows\\SysWOW64\\d3d9.dll");
 
 		if (!hL) return false;
 
-		p[0] = GetProcAddress(hL,"D3DPERF_BeginEvent");
-		p[1] = GetProcAddress(hL,"D3DPERF_EndEvent");
-		p[2] = GetProcAddress(hL,"D3DPERF_GetStatus");
-		p[3] = GetProcAddress(hL,"D3DPERF_QueryRepeatFrame");
-		p[4] = GetProcAddress(hL,"D3DPERF_SetMarker");
-		p[5] = GetProcAddress(hL,"D3DPERF_SetOptions");
-		p[6] = GetProcAddress(hL,"D3DPERF_SetRegion");
-		p[7] = GetProcAddress(hL,"DebugSetLevel");
-		p[8] = GetProcAddress(hL,"DebugSetMute");
-		p[9] = GetProcAddress(hL,"Direct3D9EnableMaximizedWindowedModeShim");
-		p[10] = GetProcAddress(hL,"Direct3DCreate9");
-		p[11] = GetProcAddress(hL,"Direct3DCreate9Ex");
-		p[12] = GetProcAddress(hL,"Direct3DShaderValidatorCreate9");
-		p[13] = GetProcAddress(hL,"PSGPError");
-		p[14] = GetProcAddress(hL,"PSGPSampleTexture");
-		p[15] = GetProcAddress(hL,(LPCSTR)LOWORD(16)); // Apparently this is some undocumented function added in the Windows 8.1 update: "DWORD Direct3D9ForceHybridEnumeration(UINT)"
-
-		}
+		p[0] = GetProcAddress(hL, "D3DPERF_BeginEvent");
+		p[1] = GetProcAddress(hL, "D3DPERF_EndEvent");
+		p[2] = GetProcAddress(hL, "D3DPERF_GetStatus");
+		p[3] = GetProcAddress(hL, "D3DPERF_QueryRepeatFrame");
+		p[4] = GetProcAddress(hL, "D3DPERF_SetMarker");
+		p[5] = GetProcAddress(hL, "D3DPERF_SetOptions");
+		p[6] = GetProcAddress(hL, "D3DPERF_SetRegion");
+		p[7] = GetProcAddress(hL, "DebugSetLevel");
+		p[8] = GetProcAddress(hL, "DebugSetMute");
+		p[9] = GetProcAddress(hL, "Direct3D9EnableMaximizedWindowedModeShim");
+		p[10] = GetProcAddress(hL, "Direct3DCreate9");
+		p[11] = GetProcAddress(hL, "Direct3DCreate9Ex");
+		p[12] = GetProcAddress(hL, "Direct3DShaderValidatorCreate9");
+		p[13] = GetProcAddress(hL, "PSGPError");
+		p[14] = GetProcAddress(hL, "PSGPSampleTexture");
+		p[15] = GetProcAddress(hL, (LPCSTR)LOWORD(16u) ); // Apparently this is some undocumented function added in the Windows 8.1 update: "DWORD Direct3D9ForceHybridEnumeration(UINT)"
+	}
 	if (reason == DLL_PROCESS_DETACH)
-		{
+	{
 		FreeLibrary(hL);
-		}
+	}
 
 	return 1;
-	}
+}
 
 // D3DPERF_BeginEvent
 extern "C" int WINAPI __E__0__(D3DCOLOR col, LPCWSTR wszName)
-	{
-		typedef int (WINAPI *D3DPERF_BeginEventType)(D3DCOLOR col, LPCWSTR wszName);
-		D3DPERF_BeginEventType beginEventPtr = (D3DPERF_BeginEventType)p[0];
-		int ret = (*beginEventPtr)(col, wszName);
-		return ret;
-	}
+{
+	typedef int (WINAPI *D3DPERF_BeginEventType)(D3DCOLOR col, LPCWSTR wszName);
+	D3DPERF_BeginEventType beginEventPtr = (D3DPERF_BeginEventType)p[0];
+	int ret = (*beginEventPtr)(col, wszName);
+	return ret;
+}
 
 // D3DPERF_EndEvent
 extern "C" int WINAPI __E__1__(void)
-	{
-		typedef int (WINAPI *D3DPERF_EndEventType)(void);
-		D3DPERF_EndEventType endEventPtr = (D3DPERF_EndEventType)p[1];
-		int ret = (*endEventPtr)();
-		return ret;
-	}
+{
+	typedef int (WINAPI *D3DPERF_EndEventType)(void);
+	D3DPERF_EndEventType endEventPtr = (D3DPERF_EndEventType)p[1];
+	int ret = (*endEventPtr)();
+	return ret;
+}
 
 // D3DPERF_GetStatus
 extern "C" DWORD WINAPI __E__2__(void)
-	{
-		typedef DWORD (WINAPI *D3DPERF_GetStatusType)(void);
-		D3DPERF_GetStatusType getStatusPtr = (D3DPERF_GetStatusType)p[2];
-		DWORD ret = (*getStatusPtr)();
-		return ret;
-	}
+{
+	typedef DWORD (WINAPI *D3DPERF_GetStatusType)(void);
+	D3DPERF_GetStatusType getStatusPtr = (D3DPERF_GetStatusType)p[2];
+	DWORD ret = (*getStatusPtr)();
+	return ret;
+}
 
 // D3DPERF_QueryRepeatFrame
 extern "C" BOOL WINAPI __E__3__(void)
-	{
-		typedef BOOL (WINAPI *D3DPERF_QueryRepeatFrameType)(void);
-		D3DPERF_QueryRepeatFrameType queryRepeatFramePtr = (D3DPERF_QueryRepeatFrameType)p[3];
-		BOOL ret = (*queryRepeatFramePtr)();
-		return ret;
-	}
+{
+	typedef BOOL (WINAPI *D3DPERF_QueryRepeatFrameType)(void);
+	D3DPERF_QueryRepeatFrameType queryRepeatFramePtr = (D3DPERF_QueryRepeatFrameType)p[3];
+	BOOL ret = (*queryRepeatFramePtr)();
+	return ret;
+}
 
 // D3DPERF_SetMarker
 extern "C" void WINAPI __E__4__(D3DCOLOR col, LPCWSTR wszName)
-	{
-		typedef void (WINAPI *D3DPERF_SetMarker)(D3DCOLOR col, LPCWSTR wszName);
-		D3DPERF_SetMarker setMarkerPtr = (D3DPERF_SetMarker)p[4];
-		(*setMarkerPtr)(col, wszName);
-	}
+{
+	typedef void (WINAPI *D3DPERF_SetMarker)(D3DCOLOR col, LPCWSTR wszName);
+	D3DPERF_SetMarker setMarkerPtr = (D3DPERF_SetMarker)p[4];
+	(*setMarkerPtr)(col, wszName);
+}
 
 // D3DPERF_SetOptions
 extern "C" void WINAPI __E__5__(DWORD dwOptions)
-	{
-		typedef void (WINAPI *D3DPERF_SetOptions)(DWORD dwOptions);
-		D3DPERF_SetOptions setOptionsPtr = (D3DPERF_SetOptions)p[5];
-		(*setOptionsPtr)(dwOptions);
-	}
+{
+	typedef void (WINAPI *D3DPERF_SetOptions)(DWORD dwOptions);
+	D3DPERF_SetOptions setOptionsPtr = (D3DPERF_SetOptions)p[5];
+	(*setOptionsPtr)(dwOptions);
+}
 
 // D3DPERF_SetRegion
 extern "C" void WINAPI __E__6__(D3DCOLOR col, LPCWSTR wszName)
-	{
-		typedef void (WINAPI *D3DPERF_SetRegion)(D3DCOLOR col, LPCWSTR wszName);
-		D3DPERF_SetRegion setRegionPtr = (D3DPERF_SetRegion)p[6];
-		(*setRegionPtr)(col, wszName);
-	}
+{
+	typedef void (WINAPI *D3DPERF_SetRegion)(D3DCOLOR col, LPCWSTR wszName);
+	D3DPERF_SetRegion setRegionPtr = (D3DPERF_SetRegion)p[6];
+	(*setRegionPtr)(col, wszName);
+}
 
 // DebugSetLevel
 extern "C" __declspec(naked) void __stdcall __E__7__()
+{
+__asm
 	{
-	__asm
-		{
-		jmp p[7*4];
-		}
+	jmp p[7*4];
 	}
+}
 
 // DebugSetMute
 extern "C" __declspec(naked) void __stdcall __E__8__()
+{
+__asm
 	{
-	__asm
-		{
-		jmp p[8*4];
-		}
+	jmp p[8*4];
 	}
+}
 
 // Direct3D9EnableMaximizedWindowedModeShim
 extern "C" __declspec(naked) void __stdcall __E__9__()
+{
+__asm
 	{
-	__asm
-		{
-		jmp p[9*4];
-		}
+	jmp p[9*4];
 	}
+}
 
 // Direct3DCreate9
 extern "C" IDirect3D9* WINAPI __E__10__(_In_ UINT SDKVersion)
-	{	
-		// Pre-hook code
-		typedef IDirect3D9* (WINAPI *Direct3DCreate9Type)(_In_ UINT SDKVersion);
-		Direct3DCreate9Type d3dCreate9Ptr = (Direct3DCreate9Type)p[10];
-		IDirect3D9* ret = (*d3dCreate9Ptr)(SDKVersion);
-		// Post-hook code
+{	
+	// Pre-hook code
+	typedef IDirect3D9* (WINAPI *Direct3DCreate9Type)(_In_ UINT SDKVersion);
+	Direct3DCreate9Type d3dCreate9Ptr = (Direct3DCreate9Type)p[10];
+	IDirect3D9* ret = (*d3dCreate9Ptr)(SDKVersion);
+	// Post-hook code
 
-		if (!ret)
-		{
-			return NULL;
-		}
-
-		IDirect3D9Hook* newHook = new IDirect3D9Hook(ret);
-		return newHook;
+	if (!ret)
+	{
+		return NULL;
 	}
+
+	IDirect3D9Hook* newHook = new IDirect3D9Hook(ret);
+	return newHook;
+}
 
 // Direct3DCreate9Ex
 extern "C" HRESULT WINAPI __E__11__(_In_ UINT SDKVersion, _Out_ IDirect3D9Ex** ppD3D)
-	{
-		// Pre-hook code
-		typedef HRESULT (WINAPI *Direct3DCreate9ExType)(_In_ UINT SDKVersion, _Out_ IDirect3D9Ex**);
-		Direct3DCreate9ExType d3dCreate9ExPtr = (Direct3DCreate9ExType)p[11];
-		HRESULT ret = (*d3dCreate9ExPtr)(SDKVersion, ppD3D);
-		// Post-hook code
-		//MessageBoxA(NULL, "Direct3DCreate9Ex", NULL, NULL);
-		return ret;
-	}
+{
+	// Pre-hook code
+	typedef HRESULT (WINAPI *Direct3DCreate9ExType)(_In_ UINT SDKVersion, _Out_ IDirect3D9Ex**);
+	Direct3DCreate9ExType d3dCreate9ExPtr = (Direct3DCreate9ExType)p[11];
+	HRESULT ret = (*d3dCreate9ExPtr)(SDKVersion, ppD3D);
+	// Post-hook code
+	//MessageBoxA(NULL, "Direct3DCreate9Ex", NULL, NULL);
+	return ret;
+}
 
 // Direct3DShaderValidatorCreate9
 extern "C" __declspec(naked) void __stdcall __E__12__()
+{
+__asm
 	{
-	__asm
-		{
-		jmp p[12*4];
-		}
+	jmp p[12*4];
 	}
+}
 
 // PSGPError
 extern "C" __declspec(naked) void __stdcall __E__13__()
+{
+__asm
 	{
-	__asm
-		{
-		jmp p[13*4];
-		}
+	jmp p[13*4];
 	}
+}
 
 // PSGPSampleTexture
 extern "C" __declspec(naked) void __stdcall __E__14__()
+{
+__asm
 	{
-	__asm
-		{
-		jmp p[14*4];
-		}
+	jmp p[14*4];
 	}
+}
 
 // ___XXX___16
 extern "C" __declspec(naked) void __stdcall __E__15__()
+{
+__asm
 	{
-	__asm
-		{
-		jmp p[15*4];
-		}
+	jmp p[15*4];
 	}
+}
 
